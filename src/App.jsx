dd9c667a2b2a8a275a88cbad6eaad2f4744fabc2
Giovanni Sia <76817363+GiovanniSia@users.noreply.github.com>
2024-01-30 21:59:47 -0300

    add us

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Start from "./components/start/Start";
+import Us from "./components/us/Us";
 
 function App() {
   return (
     <>
       <Start />
+      <Us />
     </>
   );
 }
---
+++ src/components/us/Us.jsx
@@ -0,0 +1,39 @@
+import section1 from "../../img/us/section1.svg";
+import section2 from "../../img/us/section2.svg";
+import "./Us.css";
+
+function Us() {
+  return (
+    <div className="us">
+      <div className="title-first-us">
+        <h1>Nosotros</h1>
+        <p>
+          Somos un grupo de estudiantes y graduados en informática <br />
+          desarrollamos su idea o solucionamos sus problemas
+        </p>
+      </div>
+
+      <div className="title-second-us">
+        <h1>¿Qué hacemos?</h1>
+        <div className="two-column">
+          <div>
+            <img src={section1} alt="" />
+            <h2>Diseño y desarrollo web</h2>
+            <p>Creación de páginas web</p>
+            <p>Rediseño y actualización de sitios web</p>
+            <p>Adaptar sitios web a diversos dispositivos</p>
+          </div>
+          <div>
+            <img src={section2} alt="" />
+            <h2>Desarrollo de software</h2>
+            <p>Creación de software personalizado</p>
+            <p>Soporte y mantenimiento de software</p>
+            <p>Desarrollo Full Stack</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Us;
